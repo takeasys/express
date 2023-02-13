f742578b5d601a6b201eac526656bd09da75d6c2
Takeasys <93749956+takeasys@users.noreply.github.com>
2023-02-13 15:48:53 +0000

    mongodb

--- db/mongodb.js
+++ db/mongodb.js
@@ -1,18 +1,17 @@
 const { MongoClient } = require("mongodb");
 const uri = process.env.ATLAS_URI
-const client = new MongoClient(uri);
 
-async function run() {
+const connectMongoDb = async (dbName, callback = () =>{}) => {
+  const client = new MongoClient(uri);
   try {
-    const database = client.db('sample_mflix');
-    const movies = database.collection('movies');
-    // Query for a movie that has the title 'Back to the Future'
-    const query = { title: 'Back to the Future' };
-    const movie = await movies.findOne(query);
-    console.log(movie);
+    const database = client.db(dbName);
+
+    callback(database);
+
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
-run().catch(console.dir);
+
+module.exports = connectMongoDb;
--- routes/articles.js
+++ routes/articles.js
@@ -1,11 +1,23 @@
 var express = require("express");
 var router = express.Router();
 const ArticleService = require("../services/article")
+const connectMongoDb = require("../db/mongodb");
 
 /* GET users listing. */
 router.get("/", async function (req, res, next) {
-    const articles = await ArticleService.getAllArticles();
-    res.send(articles)
+    // const articles = await ArticleService.getAllArticles();
+
+    await connectMongoDb("demo", async (db) => {
+        const articleConnection = await db.collection("articles");
+
+        const cursor = articleConnection.find({});
+        // await cursor.forEach(doc => console.log(doc));
+        
+        res.send(await cursor.toArray());
+
+        cursor.close();
+    });
+    
 });
 
 router.get("/add", async function(req, res, next) {
