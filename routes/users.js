1be3a3c8c90ce0016d9529b422f347f0ac094087
Takeasys <takeasys@gmail.com>
2023-02-13 17:03:18 +0800

    Revert "mongoose"
    This reverts commit 51b6a147081ed26dd1645b513b0be3b083414d64.

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,9 @@
 var express = require("express");
 var router = express.Router();
-var clientService = require("../controllers/user")
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  // res.send("respond with a resource");
-
-  const userService = new clientService();
-  const allclients = userService.getAllclients()
-
-  res.send(allclients)
+  res.send("respond with a resource");
 });
 
 module.exports = router;
